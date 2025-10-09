356306f919339ba767df1fd93d3c6bf04176e3e8
salvatore.torsello <salvatore.torsello@aresys.it>
2025-10-09 09:52:22 +0200

    nvim: fix yank to clipboard

--- nvim/.config/nvim/lua/salvo/init.lua
+++ nvim/.config/nvim/lua/salvo/init.lua
@@ -1,3 +1,15 @@
 require("salvo.set")
 require("salvo.remap")
 require("salvo.lazy_init")
+
+vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      },
+    }
