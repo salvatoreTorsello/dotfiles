8af8a59623043039e869e16a0cc6a1ef881418cd
Salvatore Torsello <salvatore.torsello@aresys.it>
2026-02-27 19:02:15 +0100

    nvim: lsp-zero restore

--- nvim/.config/nvim/lua/salvo/lazy/lsp/lsp-zero.lua
+++ nvim/.config/nvim/lua/salvo/lazy/lsp/lsp-zero.lua
@@ -6,39 +6,6 @@ return {
                 opts = {},
         },
 
-        -- Formatter integration
-        {
-                'stevearc/conform.nvim',
-                event = { 'BufWritePre' },
-                config = function()
-                        require('conform').setup({
-                                formatters_by_ft = {
-                                        c = { 'clang-format' },
-                                        cpp = { 'clang-format' },
-                                        cuda = { 'clang-format' },
-                                },
-                                format_on_save = {
-                                        timeout_ms = 1000,
-                                        lsp_fallback = false,
-                                },
-                                log_level = vim.log.levels.DEBUG,
-                        })
-
-                        -- Keymap for manual formatting
-                        vim.api.nvim_create_user_command('Format', function(args)
-                                local range = nil
-                                if args.count ~= -1 then
-                                        local end_line = vim.api.nvim_buf_get_lines(0, args.count - 1, args.count, false)
-                                        range = {
-                                                { args.line1 - 1, 0 },
-                                                { args.count - 1, #end_line[1] },
-                                        }
-                                end
-                                require('conform').format({ async = true, lsp_fallback = false, range = range })
-                        end, { range = true })
-                end,
-        },
-
         -- Autocompletion
         {
                 'hrsh7th/nvim-cmp',
@@ -48,7 +15,7 @@ return {
 
                         cmp.setup({
                                 sources = {
-                                        { name = 'nvim_lsp' },
+                                        {name = 'nvim_lsp'},
                                 },
                                 mapping = cmp.mapping.preset.insert({
                                         ['<C-Space>'] = cmp.mapping.complete(),
@@ -67,23 +34,27 @@ return {
         -- LSP
         {
                 'neovim/nvim-lspconfig',
-                cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-                event = { 'BufReadPre', 'BufNewFile' },
+                cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+                event = {'BufReadPre', 'BufNewFile'},
                 dependencies = {
-                        { 'hrsh7th/cmp-nvim-lsp' },
-                        { 'williamboman/mason.nvim' },
-                        { 'williamboman/mason-lspconfig.nvim' },
+                        {'hrsh7th/cmp-nvim-lsp'},
+                        {'williamboman/mason.nvim'},
+                        {'williamboman/mason-lspconfig.nvim'},
                 },
                 init = function()
+                        -- Reserve a space in the gutter
+                        -- This will avoid an annoying layout shift in the screen
                         vim.opt.signcolumn = 'yes'
+                        -- Set the wait time before the diagnostic window appears
                         vim.opt.updatetime = 150
 
+                        -- Configure diagnostics to appear in floating windows
                         vim.diagnostic.config({
                                 float = {
-                                        border = 'rounded',
-                                        source = 'always',
-                                        header = '',
-                                        prefix = '',
+                                        border = "rounded",
+                                        source = "always",
+                                        header = "",
+                                        prefix = "",
                                 },
                                 virtual_text = false,
                                 signs = true,
@@ -95,47 +66,40 @@ return {
                 config = function()
                         local lsp_defaults = require('lspconfig').util.default_config
 
+                        -- Add cmp_nvim_lsp capabilities settings to lspconfig
+                        -- This should be executed before you configure any language server
                         lsp_defaults.capabilities = vim.tbl_deep_extend(
                                 'force',
                                 lsp_defaults.capabilities,
                                 require('cmp_nvim_lsp').default_capabilities()
                         )
 
-                        -- Ensure clangd formatting is disabled to prevent conflicts
-                        local on_attach = function(client, bufnr)
-                                -- Disable formatting for clangd to let conform.nvim handle it
-                                if client.name == 'clangd' then
-                                        client.server_capabilities.documentFormattingProvider = false
-                                        client.server_capabilities.documentRangeFormattingProvider = false
-                                end
-
-                                local opts = { buffer = bufnr }
-
-                                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                                -- Use conform.nvim for manual formatting instead of LSP
-                                vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>Format<cr>', opts)
-                                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-                                vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-
-                                vim.api.nvim_create_autocmd("CursorHold", {
-                                        buffer = bufnr,
-                                        callback = function()
-                                                vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
-                                        end,
-                                })
-                        end
-
+                        -- LspAttach is where you enable features that only work
+                        -- if there is a language server active in the file
                         vim.api.nvim_create_autocmd('LspAttach', {
                                 desc = 'LSP actions',
                                 callback = function(event)
-                                        on_attach(vim.lsp.get_client_by_id(event.data.client_id), event.buf)
+                                        local opts = {buffer = event.buf}
+
+                                        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                                        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                                        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                                        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                                        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                                        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                                        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                                        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                                        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                                        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                                        vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+
+                                        -- Automatically show diagnostics when the cursor stops on a line
+                                        vim.api.nvim_create_autocmd("CursorHold", {
+                                                buffer = event.buf,
+                                                callback = function()
+                                                        vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})
+                                                end,
+                                        })
                                 end,
                         })
 
@@ -144,30 +108,36 @@ return {
                                         "lua_ls",
                                         "pylsp",
                                         "clangd",
+                                        -- "jinja-lsp",
                                 },
                                 handlers = {
                                         function(server_name)
-                                                require('lspconfig')[server_name].setup({
-                                                        on_attach = on_attach,
-                                                })
+                                                -- require('lspconfig')[server_name].setup({})
+                                                vim.lsp.config(server_name,{})
                                         end,
                                 }
                         })
 
-                        -- Configure pylsp
-                        require('lspconfig').pylsp.setup({
+                        -- Python LSP (pylsp) with automatic venv detection
+                        -- local lspconfig = require('lspconfig')
+                        local util = require("lspconfig/util")
+
+                        -- lspconfig.pylsp.setup {
+                        vim.lsp.config('pylsp', {
                                 on_attach = on_attach,
+                                capabilities = capabilities,
                                 settings = {
                                         pylsp = {
                                                 plugins = {
-                                                        jedi = { environment = (function()
+                                                        jedi = {environment = (function()
                                                                 local venv_path = vim.fn.getcwd() .. "/venv/bin/python"
                                                                 if vim.fn.filereadable(venv_path) == 1 then
                                                                         return venv_path
                                                                 end
-                                                                return vim.fn.exepath("python")
+                                                                return vim.fn.exepath("python") -- fallback to system Python
                                                         end)()
                                                         },
+                                                        -- Enable Black formatter
                                                         black = {
                                                                 enabled = true,
                                                                 path = "/usr/bin/black",
@@ -184,13 +154,31 @@ return {
                                                         flake8 = {
                                                                 enabled = false,
                                                                 maxLineLength = 90,
-                                                                ignore = {},
+                                                                ignore = {},   -- keep empty unless you need to skip rules
                                                         }
                                                 }
                                         }
                                 }
                         })
-                end
-        },
-}
 
+                        -- Autoformat on save
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                                pattern = { "*.py", "*.c", "*.cpp", "*.h", "*.hpp", "*.cc", "*.hh", "*.cxx", "*.hxx" },
+                                callback = function(args)
+                                        local ft = vim.bo[args.buf].filetype
+                                        if ft == "python" then
+                                                -- Synchronously format the buffer with Black
+                                                local lines = vim.api.nvim_buf_get_lines(args.buf, 0, -1, false)
+                                                local formatted = vim.fn.systemlist("black -l 80 -q -", lines)
+                                                if vim.v.shell_error == 0 then
+                                                        vim.api.nvim_buf_set_lines(args.buf, 0, -1, false, formatted)
+                                                end
+                                        else
+                                                -- fallback to lsp formatter (clangd) for c/c++
+                                                vim.lsp.buf.format({ async = false })
+                                        end
+                                end,
+                        })
+                end
+        }
+}
